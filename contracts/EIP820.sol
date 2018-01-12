22d1851a752061c71a125e53fb6ba23b1b0b5403
Jordi Baylina <jordi@baylina.cat>
2018-01-13 00:40:23 +0100

    Address added

--- contracts/EIP820.sol
+++ contracts/EIP820.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.4.18;
 import "./InterfaceImplementationRegistry.sol";
 
 contract EIP820 {
-    InterfaceImplementationRegistry interfaceImplementationRegistry = InterfaceImplementationRegistry(0x4205cEf72cE73fc282075DC64D9E290A75bd9590);
+    InterfaceImplementationRegistry interfaceImplementationRegistry = InterfaceImplementationRegistry(0x8A5cc001E6550504965816241375e44F2f8b44F0);
 
     function setInterfaceImplementation(string ifaceLabel, address impl) internal {
         interfaceImplementationRegistry.setInterfaceImplementer(this, keccak256(ifaceLabel), impl);
